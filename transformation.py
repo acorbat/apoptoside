ce3ff73c8cdc568f4dc4e5406bc6c251f810d312
acorbat <acorbat@df.uba.ar>
2019-07-22 12:12:56 -0300

    Added transformation module with function to select regions

---
+++ transformation.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def get_region(region, curve, mask, length=5):
+    """
+    Return values of curve around the beginning ('pre' region) or ending ('pos'
+     region) of mask with specified length.
+    Parameters
+    ----------
+    region : str, 'pre' or 'pos'
+        Region to be returned
+    curve : list, np.array
+        Curve of interest
+    mask : list, np.array
+        Mask of the region of interest of the curve
+    length : int, optional (default=5)
+        length of the region of interest to be returned
+
+    Returns
+    -------
+    array of curve around the region of interest
+    """
+    inds = np.where(mask)[0]
+
+    if (np.diff(inds) != 1).any():
+        return None
+
+    if region not in ['pre', 'pos']:
+        raise ValueError('Only pre or pos region.')
+    center = inds[0] if region == 'pre' else inds[-1]
+
+    start_ini = np.clip(center - length // 2, 0, len(curve))
+    end_ini = np.clip(center + length // 2, 0, len(curve))
+
+    return curve[start_ini:end_ini + 1]
