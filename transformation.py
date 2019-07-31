7a0ed29b06ef47ea78454228e6a9985be47aee5f
acorbat <acorbat@df.uba.ar>
2019-07-31 11:29:56 -0300

    Added functions for calculation of enzimatic activity from anisotropy

--- transformation.py
+++ transformation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.misc import derivative
 
 
 def get_region(region, curve, mask, length=5):
@@ -42,3 +43,46 @@ def estimate_delta_b(fluo_pre, fluo_pos):
     """Estimates delta b parameter from the pre and pos cleavage fluorescence
     """
     return fluo_pre/fluo_pos - 1
+
+
+def calculate_activity(time, anisotropy, delta_b, order=5):
+    """Calculates activity of enzyme according to anisotropy curve."""
+    time_steps = np.diff(time)
+    time_step = np.nanmin(time_steps)
+    if not all(time_steps == time_step):
+        time, anisotropy = fill_in_curves(time, anisotropy)
+
+    def this_vect(t):
+        ind = t // time_step
+        ind = np.clip(ind, 0, len(anisotropy) - 1)
+        return anisotropy[ind]
+
+    der = derivative(this_vect, time, dx=time_step, order=order)
+
+    anisotropy_normalized = normalize(anisotropy)
+    activity = der / ((1 + delta_b * anisotropy_normalized) ** 2)
+
+    return time, activity
+
+
+def fill_in_curves(time, curve):
+    """Fill in the missing values with interpolation to get equispaced curve"""
+    start = np.nanmin(time)
+    end = np.nanmax(time)
+    time_step = np.nanmin(np.diff(time))
+    complete_time = np.arange(start, end, time_step)
+
+    complete_curve = np.interp(complete_time, time, curve,
+                               left=curve[0], right=curve[0])
+
+    return complete_time, complete_curve
+
+
+def normalize(vect):
+    """
+    Returns a normalization of vect from 0 to 1.
+    """
+    this_vect = vect[:]
+    this_vect -= np.nanmin(this_vect)
+    this_vect /= np.nanmax(this_vect)
+    return this_vect
