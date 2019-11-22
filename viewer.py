d419ce130244d78071ab0a4fe4beb22cf138392e
acorbat <acorbat@df.uba.ar>
2019-11-22 12:06:08 -0300

    Added documentation for viewer module.

--- viewer.py
+++ viewer.py
@@ -9,10 +9,21 @@ from matplotlib.widgets import RectangleSelector, Button
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.patheffects as pe
 
-import transformation as tf
-
 
 def df_viewer(df, sensors, save_dir):
+    """DataFrame viewer lets you see the results in a DataFrame and reselect
+    the sigmoid regions for posterior analysis.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame containing all the results to be shown
+    sensors : pandas.DataFrame
+        DataFrame containing the information pertaining the sensors present and
+         colors to be used for representation
+    save_dir : pathlib.Path
+        path where to save the modified DataFrame
+    """
 
     fig, axs = plt.subplots(5, 1,
                             figsize=(8, 10),
@@ -20,6 +31,27 @@ def df_viewer(df, sensors, save_dir):
                             sharex=True)
 
     class SubPlot(object):
+        """This subplots class keeps record of axes and data to enable fast
+        replotting. More than one column is plotted per subplot.
+
+        Attributes
+        ----------
+        axs : matplotlib.Axes
+            Axes where to plot
+        df_row : DataFrame.Series
+            row of DataFrame to be ploted
+        sensors : pandas.DataFrame
+            DataFrame with the information of sensors suchs as names and colors
+        x_col : string
+            name of the column to use as x axis
+        y_col_suffix : string
+            suffix to be appended to the fluorophore name to get the y axis col
+
+        Methods
+        -------
+        update(df_row): Replots the subplot with the given pandas.Series data.
+        set_title(df_row): Retitles the plot with the given information.
+        """
 
         def __init__(self, axs, df_row, sensors,
                      x_col='time', y_col_suffix='anisotropy'):
@@ -52,6 +84,7 @@ def df_viewer(df, sensors, save_dir):
             plt.draw()
 
         def update(self, df_row):
+            """Replots the subplot with the given pandas.Series data."""
             plt.sca(self.axs)
             time = df_row[self.x_col]
             mask = df_row.sigmoid_mask
@@ -72,6 +105,7 @@ def df_viewer(df, sensors, save_dir):
             plt.draw()
 
         def set_title(self, df_row):
+            """Retitles the plot with the given information."""
             ind = df_row.name
             date = df_row.date
             drug = df_row.drug
@@ -80,6 +114,24 @@ def df_viewer(df, sensors, save_dir):
                                (ind, date, drug, plasmid))
 
     class SimpleSubPlot(object):
+        """This subplots class keeps record of axes and data to enable fast
+        replotting. Only one column is plotted per subplot.
+
+        Attributes
+        ----------
+        axs : matplotlib.Axes
+            Axes where to plot
+        df_row : DataFrame.Series
+            row of DataFrame to be ploted
+        x_col : string
+            name of the column to use as x axis
+        y_col : string
+            name of the column to use as y axis
+
+        Methods
+        -------
+        update(df_row): Replots the subplot with the given pandas.Series data.
+        """
 
         def __init__(self, axs, df_row, x_col, y_col):
             self.axs = axs
@@ -99,6 +151,7 @@ def df_viewer(df, sensors, save_dir):
             plt.draw()
 
         def update(self, df_row):
+            """Replots the subplot with the given pandas.Series data."""
             plt.sca(self.axs)
             x = df_row[self.x_col]
             y = df_row[self.y_col]
@@ -125,6 +178,27 @@ def df_viewer(df, sensors, save_dir):
                 subplot_solidity]
 
     class Index(object):
+        """Index class keeps track of the index of the DataFrame that is being
+        worked on and calls for the replotting of every subplot, as well as
+        modifying the DataFrame.
+
+        Attributes
+        ----------
+        df : pandas.DataFrame
+            DataFrame to be worked on.
+        subplots : list of Subplots
+            list of all the subplots to be updated on each index change.
+
+        Methods
+        -------
+        next(event): Next index.
+        prev(event): Previous index.
+        get_mask(): Generates a boolean mask from RectangleSelector.
+        load(event_press, event_release): Modifies the actual mask attribute.
+        add_region(event): Adds the actual_mask to the DataFrame.
+        remove_region(event): Sets the boolean mask in DataFrame to all False.
+        save_all(event): Saves the DataFrame to the selected path.
+        """
 
         def __init__(self, df, subplots):
             self.df = df
@@ -219,6 +293,7 @@ def df_viewer(df, sensors, save_dir):
 
 
 def view_curves(axs, df_row, sensors, lines=None, fill=None):
+    """If there si no use for this, it is going to be deleted."""
     time = df_row.time
     mask = df_row.sigmoid_mask
 
@@ -264,6 +339,7 @@ def view_curves(axs, df_row, sensors, lines=None, fill=None):
 
         return lines, fill
 
+
 def plot_delta_b_histogram(df, sensors):
     """Plots an histogram for the delta b in the df. Must have a 'b' column"""
     for _, sensor_row in sensors.iterrows():
@@ -281,7 +357,8 @@ def plot_delta_b_histogram(df, sensors):
 
 def plot_histogram_2d(df, sensors, kind='histogram_2d', hue=None,
                       cols=["BFP_to_Cit", "BFP_to_Kate"]):
-    """Generates a 2D histogram of the given DataFrame and sensor dictionary."""
+    """Generates a 2D hexbin histogram or scatetr of the given DataFrame and
+    sensor dictionary."""
     x_data_col, y_data_col = cols
 
     df_fil = df.query('is_single_apoptotic')
@@ -315,7 +392,9 @@ def plot_histogram_2d(df, sensors, kind='histogram_2d', hue=None,
     g.ax_joint.axvline(x=0, ls='--', color='k', alpha=0.3)
 
 
-def make_report(pdf_dir, df, sensors, hue=None, cols=["BFP_to_Cit", "BFP_to_Kate"]):
+def make_report(pdf_dir, df, sensors, hue=None,
+                cols=["BFP_to_Cit", "BFP_to_Kate"]):
+    """Generates a pdf with all the important results of the analysis."""
     with PdfPages(str(pdf_dir)) as pp:
         plot_delta_b_histogram(df, sensors)
         pp.savefig()
@@ -352,7 +431,21 @@ def get_level_for_kde(x, y, level):
 
 def plot_joint_kde(g, df, cols=["BFP_to_Cit", "BFP_to_Kate"], label=None,
                    my_lvls=[0.34, 0.68]):
+    """Plots a specific df in the kdeplot.
 
+    Parameters
+    ----------
+    g : seaborn.JointGrid
+        Grid where to plot the kde.
+    df : pandas.DataFrame
+        DataFrame containing the data.
+    cols : list of strings
+        Names of the columns of the Dataframe to use as x and y axis.
+    label : string
+        label to name the plot.
+    my_lvls : float or list of floats
+        levels of contour curves to surround the data.
+    """
     # Filter dataset
     my_df = df.dropna(subset=cols)
 
@@ -373,7 +466,19 @@ def plot_joint_kde(g, df, cols=["BFP_to_Cit", "BFP_to_Kate"], label=None,
 
 def plot_distributions(df, cols=["BFP_to_Cit", "BFP_to_Kate"], groupby=None,
                   my_lvls=[0.34, 0.68]):
+    """Plots the distributions in a kde plot.
 
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame containing the data.
+    cols : list of string
+        names of columns in df that contain the x and y axis.
+    groupby : string or list of strings
+        list of columns to group the DataFrame.
+    my_lvls : float or list of floats
+        Contours surrounding data.
+    """
     g = sns.JointGrid(cols[0], cols[1], df)
 
     if groupby:
@@ -398,6 +503,9 @@ def plot_distributions(df, cols=["BFP_to_Cit", "BFP_to_Kate"], groupby=None,
 
 
 def plot_curves(df, sensors):
+    """Centers the curves of the three sensors to the first one that reaches
+    maximum activity. It then calculates the average curves and plots the mean
+    and 25 and 75 percentiles."""
     fine_time = np.arange(-60, 60, 1)
     curves = {fluo: [] for fluo in sensors.fluorophore.values}
     for ind, row in df.iterrows():
@@ -416,8 +524,7 @@ def plot_curves(df, sensors):
             # anisotropy = tf.normalize(anisotropy[mask])
             anisotropy = anisotropy[mask]
             curves[fluo].append(interpolate(fine_time, time[mask], anisotropy))
-            anisotropy = interpolate(fine_time, time[mask], anisotropy)
-
+            
             plt.plot(fine_time, anisotropy, color=this_fluo.color, alpha=0.1)
 
     for fluo_ind, this_fluo in sensors.iterrows():
