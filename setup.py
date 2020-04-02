953160daf15339009b9d669771b8f671f1701ad5
acorbat <acorbat@df.uba.ar>
2020-04-02 16:48:19 -0300

    Added a setup for formal installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apoptoside',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/acorbat/apoptoside/tree/master/',
+    license='MIT',
+    author='Agustin Corbat',
+    author_email='acorbat@df.uba.ar',
+    description='Package used for analysing homoFRET experiments with caspase '
+                'biosensors.',
+    install_requires=['matplotlib', 'numpy', 'pandas', 'scipy', 'seaborn']
+)
